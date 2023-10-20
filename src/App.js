83abf3b4b1c47a83d5f3994ae5095c2db8b872f4
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-20 16:58:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <h1>Increment/Decrement counter</h1>
+        <h4>using React and Redux</h4>
+
+        <div className='quantity'>
+          <a className='quantity__minus' title='Decrement'><span> - </span></a>
+          <input name="quantity" type="text" className='quantity__input' value="0" />
+          <a className='quantity__plus' title="Increment"><span> + </span></a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
