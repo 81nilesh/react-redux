335bea6b1243c2ad8918c0e26676f7a14b6273c0
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-30 16:58:27 +0530

    react-redux is completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from './actions/index';
 
 function App() {
   const myState = useSelector((state) => state.changeTheNumber)
+  const dispatch = useDispatch();
   return (
     <>
       <div className='container'>
@@ -10,13 +12,15 @@ function App() {
         <h4>using React and Redux</h4>
 
         <div className='quantity'>
-          <a className='quantity__minus' title='Decrement'><span> - </span></a>
-          <input name="quantity" type="text" className='quantity__input' value="0" />
-          <a className='quantity__plus' title="Increment"><span> + </span></a>
+          <a className='quantity__minus' title='Decrement'
+            onClick={() => dispatch(decNumber(5))}><span> - </span></a>
+          <input name="quantity" type="text" className='quantity__input' value={myState} />
+          <a className='quantity__plus' title="Increment"
+            onClick={() => dispatch(incNumber(5))}><span> + </span></a>
         </div>
       </div>
     </>
   );
 }
 
-export default App;
+export default App; 
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
 
-import rootReducer from "./reducers/index";
 
-const store = createStore(rootReducer);
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSIONS__ && window.__REDUX_DEVTOOL_EXTENSION__());
 
 export default store;
